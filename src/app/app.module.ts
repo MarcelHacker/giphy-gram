7f6cc4ce4ae516bb68f76f12abf70ee7b3de427a
Marcel Hacker <marcelhacker@dev.com>
2022-02-01 18:30:51 +0100

    Add service in providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GifsComponent } from './gifs/gifs.component';
+import { GifsService } from './services/gifs.service';
 
 @NgModule({
   declarations: [AppComponent, GifsComponent],
   imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  providers: [GifsService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
