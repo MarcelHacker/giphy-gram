b26263a44f72a19518098a003a492fe04f227aa9
Marcel Hacker <marcelhacker@dev.com>
2022-02-01 18:18:36 +0100

    Add gifs object test

--- src/app/gifs/gifs.component.ts
+++ src/app/gifs/gifs.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-gifs',
   templateUrl: './gifs.component.html',
-  styleUrls: ['./gifs.component.css']
+  styleUrls: ['./gifs.component.css'],
 })
 export class GifsComponent implements OnInit {
+  title = 'Liste von Gifs';
+  gifs = ['gif1', 'gif2', 'gif3'];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
