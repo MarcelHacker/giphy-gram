0a8959c5217a189a34092ba59b3c43ffcae9e5e5
Marcel Hacker <marcelhacker@dev.com>
2022-02-02 17:22:19 +0100

    Add ngrx

--- src/app/gifs/gifs.component.ts
+++ src/app/gifs/gifs.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { GifsService } from '../services/gifs.service';
+import { Gif } from '../interface/gif';
 
 @Component({
   selector: 'app-gifs',
@@ -7,12 +8,25 @@ import { GifsService } from '../services/gifs.service';
   styleUrls: ['./gifs.component.css'],
 })
 export class GifsComponent implements OnInit {
+  public gifs = {};
+
   constructor(private service: GifsService) {}
 
   ngOnInit(): void {
     this.service.getGifs().subscribe((result: Object) => {
       console.log('Result:' + result);
       console.table(result);
+      result = this.gifs;
     });
+  }
+
+  showData() {
+    return [
+      {
+        embed_url: 'https://giphy.com/embed/Ya2z1WUnZFK5ijJAkB',
+      },
+      { embed_url: 'https://giphy.com/embed/TBOvwBGkQShnq' },
+      { embed_url: 'https://giphy.com/embed/uypubDRjnv0icWp2hV' },
+    ];
   }
 }
