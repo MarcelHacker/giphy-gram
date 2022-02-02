ac5172bdeb491feee37408d17ca5277a7ef64813
Marcel Hacker <marcelhacker@dev.com>
2022-02-02 14:40:17 +0100

    Add interfaces

--- src/app/services/gifs.service.ts
+++ src/app/services/gifs.service.ts
@@ -4,14 +4,14 @@ import { Observable } from 'rxjs';
 import { Gif } from '../interface/gif';
 export class GifsService {
   public search = '';
-  public api_keay = '';
-  public limit = '';
+  public api_key = 'vf5nDm11F3X2Pe63jlGjWWPiFCFCZXM8';
+  public limit = '5';
 
   constructor(private http: HttpClient) {}
 
   getGifs(): Observable<Gif[]> {
     return this.http.get<Gif[]>(
-      `http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=YOUR_API_KEY&limit=5`
+      `http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=${this.api_key}&limit=${this.limit}`
     );
   }
 }
