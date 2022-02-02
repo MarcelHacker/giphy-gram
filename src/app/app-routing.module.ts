358d69dbc355b653057bc92cdf27075428668275
Marcel Hacker <marcelhacker@dev.com>
2022-02-02 18:24:55 +0100

    Add favorites route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { GifsComponent } from './gifs/gifs.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: GifsComponent },
+  { path: '/favorites', component: FavoritesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
