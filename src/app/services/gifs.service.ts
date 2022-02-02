c0ab183756ff491260d77bde26cb95ca45f67e37
Marcel Hacker <marcelhacker@dev.com>
2022-02-02 14:34:39 +0100

    Add interfaces

--- src/app/services/gifs.service.ts
+++ src/app/services/gifs.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+
+import { Gif } from '../interface/gif';
 export class GifsService {
   public search = '';
   public api_keay = '';
@@ -7,8 +9,8 @@ export class GifsService {
 
   constructor(private http: HttpClient) {}
 
-  getGifs(): Observable<Gifs[]> {
-    return this.http.get<Gifs[]>(
+  getGifs(): Observable<Gif[]> {
+    return this.http.get<Gif[]>(
       `http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=YOUR_API_KEY&limit=5`
     );
   }
