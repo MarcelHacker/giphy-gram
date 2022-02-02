f6b1e4f894ab7d36e45be079bd390bfcf7842a68
Marcel Hacker <marcelhacker@dev.com>
2022-02-02 15:24:24 +0100

    Add api key

--- src/app/services/gifs.service.ts
+++ src/app/services/gifs.service.ts
@@ -7,7 +7,7 @@ import { Gif } from '../interface/gif';
 @Injectable()
 export class GifsService {
   public search = '';
-  public api_key = 'VnF5KY4LRWTrdCIlHNdNXWjMKN9BSPxL';
+  public api_key = 'VnF5KY4LRWTrdCIlHNdNXWjMKN9BSPxL'; // created own key, because old doesn't work
   public limit = '5';
   public url = `http://api.giphy.com/v1/gifs/search?q=arnold&api_key=${this.api_key}&limit=${this.limit}`;
 
